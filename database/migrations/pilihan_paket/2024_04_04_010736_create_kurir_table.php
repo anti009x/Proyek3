6f4fbfb5e45ae2844f258f76230cc1144fe5e4e2
anti009x <riskisuleman76@gmail.com>
2024-04-04 08:15:52 +0700

    Migration Sturktur

---
+++ database/migrations/pilihan_paket/2024_04_04_010736_create_kurir_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\InputPesanan;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kurir', function (Blueprint $table) {
+            $table->id();
+            $table->string('Nama_Kurir');
+            $table->string('Nomor_Telepon');
+            $table->string('Alamat');
+            $table->foreignId(InputPesanan::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kurir');
+    }
+};
