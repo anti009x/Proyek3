7d16b65eb1e2b9760ab095dcc56bdf8835c169e9
anti009x <riskisuleman76@gmail.com>
2024-06-13 11:05:04 +0700

    Update Scure

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -16,8 +16,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/css/tables.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('assets/css/styles.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('assets/css/tables.css') }}">
 
 </head>
 
@@ -114,8 +114,8 @@
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
         crossorigin="anonymous"></script>
     {{-- <script src="{{ secure_asset('assets/js/main.js') }}"></script> --}}
-    <script src="{{ asset('assets/js/main.js') }}"></script>
-    <script src="{{ asset('assets/js/tables.js') }}"></script>
+    <script src="{{ secure_asset('assets/js/main.js') }}"></script>
+    <script src="{{ secure_asset('assets/js/tables.js') }}"></script>
 
 </body>
 
